df9aad3b1848f741638f0a0b8a7ee37dc729f734
Sheth Uday <shethuday505@gmail.com>
2026-01-28 13:32:05 +0530

    Created user seeder

---
+++ database/seeders/userSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
+
+class userSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 51; $i++) {
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => Hash::make($faker->password),
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
+    }
+}
